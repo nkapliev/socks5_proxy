cd72bb54848dbf5487913e54357596ca948d4927
nkapliev <nkapliev@gmail.com>
2017-07-01 20:27:43 +0100

    Add env configuration

--- main.go
+++ main.go
@@ -1,19 +1,45 @@
 package main
 
 import (
-	"github.com/armon/go-socks5"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/nkapliev/go-socks5"
+	"log"
+	"fmt"
 )
 
+type AppConfig struct {
+	ListenTransport      string `default:"tcp",split_words:"true"`
+	ListenIp      	     string `default:"",split_words:"true"`
+	ListenPort           string `default:"1080",split_words:"true"`
+	Login                string `required:"true",split_words:"true"`
+	Password             string `required:"true",split_words:"true"`
+}
+
+const CONFIG_ENV_PREFIX = "SOCKS5_PROXY"
+
+func get_socks5_config(appConfig *AppConfig) *socks5.Config {
+	cred := socks5.StaticCredentials{appConfig.Login: appConfig.Password}
+	authenticator := socks5.UserPassAuthenticator{Credentials: cred}
+	return &socks5.Config{AuthMethods: []socks5.Authenticator{authenticator}}
+}
+
 func main() {
-	// Create a SOCKS5 server
-	conf := &socks5.Config{}
-	server, err := socks5.New(conf)
+	var appConfig AppConfig
+	if err := envconfig.Process(CONFIG_ENV_PREFIX, &appConfig); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	format := "ListenTransport: %s\nListenIp: %s\nListenPort: %s\nLogin: %s\nPassword: %s\n"
+	fmt.Printf(format, appConfig.ListenTransport, appConfig.ListenIp, appConfig.ListenPort, appConfig.Login, appConfig.Password)
+
+	socks5Config := get_socks5_config(&appConfig)
+	server, err := socks5.New(socks5Config)
 	if err != nil {
 		panic(err)
 	}
 
-	// Create SOCKS5 proxy on localhost port 8000
-	if err := server.ListenAndServe("tcp", "127.0.0.1:8000"); err != nil {
+	addr := appConfig.ListenIp + ":" + appConfig.ListenPort
+	if err := server.ListenAndServe(appConfig.ListenTransport, addr); err != nil {
 		panic(err)
 	}
 }
